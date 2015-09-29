d560bc74b86544d3678ce04f20362e819e104689
xiaoli2018 <xiaoli000058@gmail.com>
2015-09-29 11:10:05 +0800

    the first

---
+++ KNN.py
@@ -0,0 +1,132 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'liheng'
+#k-近邻算法
+from numpy import *
+import operator
+from os import listdir
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group,labels
+#进行算法实现
+def classify0(inX,dataSet,labels,k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX,(dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()   # 对计算好的距离进行排序，以传入的数组的索引进行排序
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]  #将对应的索引返回给labels来匹配对应的种类
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   #get方法可以设置键的默认值
+    sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1),reverse=True)  #sorted对key中指定的元素进行排序，operator.itemgetter(1)用来提取前面的数组中的元素
+    return sortedClassCount[0][0]
+#导入文件数据
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]   #返回的是行的个数
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+#测试算法的错误率
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d ,the real answer is : %d"%(classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is : %f "%(errorCount/float(numTestVecs))
+
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat,datingLabels = file2matrix("datingTestSet2.txt")
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    inArr = array((ffMiles,percentTats,iceCream))
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "You will probably like this person:",resultList[classifierResult - 1]
+
+#32*32的矩阵转化成1*1024的向量
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+#手写数字识别系统的测试代码
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    #提取出训练集
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  #将文件名提取出来
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s'%fileNameStr)
+
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    #提取训练集并用算法提取预测结果
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s'%fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print "the classifier came back with : %d,the real answer is: %d"%(classifierResult,classNumStr)
+        if(classifierResult != classNumStr):
+            errorCount += 1.0
+    print "the total number of errors is :%d"%errorCount
+    print "the total error rate is : %f"%(errorCount/float(mTest))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
